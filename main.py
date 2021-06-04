e7a8dedac79771857af27bb3807ec7692ff1558b
JL <jlsangir@gmail.com>
2021-06-04 09:27:18 +0200

    Se añade menu superior

--- main.py
+++ main.py
@@ -8,11 +8,48 @@ window.geometry('350x200')
 lbl = Label(window, text="Hello")
 lbl.grid(column=0, row=0)
 
+
+def donothing():
+    filewin = Toplevel(window)
+    button = Button(filewin, text="Do nothing button")
+    button.pack()
+
+
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_command(label="Save as...", command=donothing)
+filemenu.add_command(label="Close", command=donothing)
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=window.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo", command=donothing)
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut", command=donothing)
+editmenu.add_command(label="Copy", command=donothing)
+editmenu.add_command(label="Paste", command=donothing)
+editmenu.add_command(label="Delete", command=donothing)
+editmenu.add_command(label="Select All", command=donothing)
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
 def clicked():
     lbl.configure(text="Button was clicked !!")
 
-btn = Button(window, text="Click Me", command=clicked)
+btn = Button(window, text="Pulsame", command=clicked)
 btn.grid(column=1, row=0)
 
+window.config(menu=menubar)
 window.mainloop()
 
