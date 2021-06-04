0d33ec46c79733323bbe835f5d9d29e1f75a3e87
jlsanCasa <jlsangir@gmail.com>
2021-06-04 18:32:42 +0200

    Corrige errores anteriores

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from menudef import *
 from tkinter import *
 from exceldef import *
 
-
 window = Tk()
 
 window.title("NAVE DE GRAPAS")
 window.geometry('600x400')
 
+
 def clicked():
-    lbl.configure(text=texto)
     height = 5
     width = 5
-    for i in range(height): #Rows
-        for j in range(width): #Columns
-            b = Entry(window, text=type(lista_sheets))
+    for i in range(height):  # Rows
+        for j in range(width):  # Columns
+            b = Entry(window, text=type(lbl))
             b.grid(row=i, column=j)
+
 
 ts = ver_sheets()
 tsa = ts[1]
@@ -27,6 +27,4 @@ btn.grid(column=1, row=0)
 menubar = creamenu(window)
 window.config(menu=menubar)
 
-
 window.mainloop()
-
