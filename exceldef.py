01565efa9e2859c63d2a00c063a16d6cf52875d2
JL <jlsangir@gmail.com>
2021-06-09 15:02:37 +0200

    Ajusta el grid con colores y tamaños

--- exceldef.py
+++ exceldef.py
@@ -2,10 +2,10 @@
 
 from openpyxl import *
 
-def ver_sheets():
+def ver_sheets(namesheet):
     #wb = Workbook()
     excel_doc = load_workbook('C:\DatosF\partesproduccion.xlsm')
-    sheet = excel_doc.get_sheet_by_name('EstadoProducc')
+    sheet = excel_doc.get_sheet_by_name(namesheet)
     all_rows = sheet.iter_rows(min_row=1, max_row=10,values_only=True)
     return all_rows
 
--- main.py
+++ main.py
@@ -5,20 +5,26 @@ from exceldef import *
 window = Tk()
 
 window.title("NAVE DE GRAPAS")
-window.geometry('600x400')
+window.geometry('1600x400')
 
 
 def clicked():
     height = 10
     width = 8
-    ts = ver_sheets()
+    ts = ver_sheets('EstadoProducc')
     j = 0
     i = 0
     for tsa in ts:
         for v in tsa:
-            b = Entry(window, width=8)
-            b.grid(row=i + 30, column=20 + j)
-            b.insert(0, '{}'.format(v))
+            if v != None:
+                bcolor = "blue"
+                fcolor = "white"
+                b = Entry(window, width=10, background=bcolor, foreground=fcolor)
+                b.grid(row=i + 30, column=20 + j)
+                b.insert(0, '{:>10}'.format(v))
+            else:
+                i += 1
+                break
             j += 1
         i += 1
         j = 0
