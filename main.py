4a549f4f0871801619759fad4bf5d7ecf3aee1d2
JL <jlsangir@gmail.com>
2021-06-16 14:39:48 +0200

    Añade clase ventanas. Optimiza menus

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 
-from tkinter import *
 from ventana import *
-
-
-def muestratemp():
-    print("aqui")
-    Ventanas.ventanaNueva()
-
-
-
-#btn = Button(window, text="VER PRODUCCIÓN", command=verproducc)
-#btn.grid(column=1, row=0)
-
 
 def main():
     mi_app = Aplicacion()
--- ventana.py
+++ ventana.py
@@ -8,9 +8,7 @@ class Aplicacion():
     posx_y = 0
 
     def __init__(self):
-        ''' Construye ventana de aplicación '''
-
-        # Declara ventana de aplicación
+        ''' Construye la ventana  principal aplicación '''
 
         self.raiz = Tk()
         self.raiz.title("NAVE DE GRAPAS")
@@ -34,56 +32,24 @@ class Aplicacion():
         self.m.add_separator()
         self.m.add_command(label="Rename")
 
-        #boton = ttk.Button(self.raiz, text='Abrir', command=self.abrir)
-        #boton.pack(side=BOTTOM, padx=20, pady=20)
         self.raiz.mainloop()
 
     def abrir(self):
         ''' Construye una ventana de diálogo '''
-
-        # Define una nueva ventana de diálogo
-
         self.dialogo = Toplevel()
-
-        # Incrementa en 1 el contador de ventanas
-
         Aplicacion.ventana += 1
-
-        # Recalcula posición de la ventana
-
         Aplicacion.posx_y += 50
         tamypos = '200x100+' + str(Aplicacion.posx_y) + \
                   '+' + str(Aplicacion.posx_y)
         self.dialogo.geometry(tamypos)
         self.dialogo.resizable(0, 0)
-
-        # Obtiene identicador de la nueva ventana
-
         ident = self.dialogo.winfo_id()
-
-        # Construye mensaje de la barra de título
 
         titulo = str(Aplicacion.ventana) + ": " + str(ident)
         self.dialogo.title(titulo)
-
-        # Define el botón 'Cerrar' que cuando sea
-        # presionado cerrará (destruirá) la ventana
-        # 'self.dialogo' llamando al método
-        # 'self.dialogo.destroy'
-
         boton = ttk.Button(self.dialogo, text='Cerrar',
                            command=self.dialogo.destroy)
         boton.pack(side=BOTTOM, padx=20, pady=20)
-
-        # Cuando la ejecución del programa llega a este
-        # punto se utiliza el método wait_window() para
-        # esperar que la ventana 'self.dialogo' sea
-        # destruida.
-        # Mientras tanto se atiende a los eventos locales
-        # que se produzcan, por lo que otras partes de la
-        # aplicación seguirán funcionando con normalidad.
-        # Si hay código después de esta línea se ejecutará
-        # cuando la ventana 'self.dialogo' sea cerrada.
 
         self.raiz.wait_window(self.dialogo)
 
