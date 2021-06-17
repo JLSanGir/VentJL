e6fc347b563ebb54b73bccd4914761496c2a7d8b
JL <jlsangir@gmail.com>
2021-06-17 08:34:58 +0200

    Añade clase ventanas. Menus pop_up OK. Add ventanas.py

--- exceldef.py
+++ exceldef.py
@@ -1,13 +1,21 @@
 
-
 from openpyxl import *
 
-def ver_sheets():
-    #wb = Workbook()
-    wb = load_workbook('C:\DatosF\partesproduccion.xlsm')
-    return wb.sheetnames
 
+def ver_sheets(namesheet):
+    excel_doc = load_workbook('C:\DatosF\partesproduccion.xlsm')
+    sheet = excel_doc.get_sheet_by_name(namesheet)
 
+    ultimafilahoja = sheet.max_row
+    ultimafila = 1
+    for r in range(1, ultimafilahoja):
+        if not sheet.cell(row=r, column=3).value is None:
+            ultimafila = r + 1
+        else:
+            break
+
+    all_rows = sheet.iter_rows(min_row=1, max_row=ultimafilahoja,values_only=True)
+    return all_rows, ultimafilahoja
 
 
 # grab the active worksheet
--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-from menudef import *
-from tkinter import *
-from exceldef import *
 
-window = Tk()
+from ventana import *
 
-window.title("NAVE DE GRAPAS")
-window.geometry('600x400')
+def main():
+    mi_app = Aplicacion()
+    return (0)
 
 
-def clicked():
-    height = 5
-    width = 5
-    for i in range(height):  # Rows
-        for j in range(width):  # Columns
-            b = Entry(window, text=type(lbl))
-            b.grid(row=i, column=j)
-
-
-ts = ver_sheets()
-tsa = ts[1]
-lbl = Label(window, text=tsa)
-lbl.grid(column=5, row=20)
-btn = Button(window, text="Pulsame", command=clicked)
-btn.grid(column=1, row=0)
-
-menubar = creamenu(window)
-window.config(menu=menubar)
-
-window.mainloop()
+if __name__ == '__main__':
+    main()
